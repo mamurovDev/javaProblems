e3e7f8975eaf6ea408d33ac05ee4bc51ee889826
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-06 20:14:03 +0500

    loops | task 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int d = scanner.nextInt();
+
+        for (int i = 0; i <= d; i++){
+            System.out.println(i + " ");
+        }
+    }
+}
