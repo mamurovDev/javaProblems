2561c86f034a5619a93993cd6d16d86698d6c94c
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-08 12:02:33 +0500

    FoP_T10_Practice_07

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
-
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        System.out.print(a == b && b == c && c == a);
-    }
+
+class Main {
 
 
+        public static int compareRectangles(double x1, double y1, double x2, double y2) {
+            double area1 = x1 * y1;
+            double area2 = x2 * y2;
+
+            if (area1 == area2) {
+                return 0; // The areas are equal
+            } else if (area1 > area2) {
+                return 1; // Rectangle 1 has a greater area
+            } else {
+                return -1; // Rectangle 2 has a greater area
+            }
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            double x1 = sc.nextDouble(); // Replace with the actual values for rectangle 1
+            double y1 = sc.nextDouble();
+            double x2 = sc.nextDouble(); // Replace with the actual values for rectangle 2
+            double y2 = sc.nextDouble();
+
+            int result = compareRectangles(x1, y1, x2, y2);
+            System.out.println(result);
+        }
 
 }
