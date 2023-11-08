2f54f6903d39c895f36c9e39aa1a1d2dbfa47298
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-08 11:26:40 +0500

    FoP_T10_Practice_05

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,38 @@
-import java.util.Scanner;
 
-class Main {
+import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        double a = scanner.nextDouble(); // Replace with your value for 'a'
+        double b = scanner.nextDouble(); // Replace with your value for 'b'
+        double x = scanner.nextDouble(); // Replace with your value for 'x'
 
-        if (a * a <= b * b) {
-            System.out.println(a);;
-        } else {
-            System.out.println(b);;
-        }
-        scanner.close();
+        // Test Case 1
+        double result1 = calculateAndRoundAbsoluteValue(a, b, x);
+        System.out.printf("%.3f%n", result1);
+
     }
 
+    public static double calculateAndRoundAbsoluteValue(double a, double b, double x) {
+        double result = a * x + b;
+
+        if (result < 0) {
+            result = -result;
+        }
+
+        double roundedResult = roundToThreeDecimalPlaces(result);
+        return roundedResult;
+    }
+
+    public static double roundToThreeDecimalPlaces(double num) {
+        double multiplier = 1000.0;
+        double roundedNum = num * multiplier;
+
+        roundedNum += (num < 0) ? -0.5 : 0.5;
+
+        int roundedInt = (int) roundedNum;
+
+        return roundedInt / multiplier;
+    }
 
 }
