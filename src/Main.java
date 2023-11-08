61a4754ef600e12610d715dc2c5635df329d288d
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-08 12:37:07 +0500

    FoP_T10_2_Practice_02

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
 
-        System.out.print("Enter a floating-point number: ");
-        double a = scanner.nextDouble();
-
-        String result;
-
-        if (a >= 0) {
-            result = formatResult(square(a));
-        } else {
-            result = formatResult(fourthPower(a));
+        while(d >= 0){
+            System.out.print(d + " ");
+            d--;
         }
-
-        System.out.println(result);
     }
 
-    public static double square(double number) {
-        return number * number;
-    }
-
-    public static double fourthPower(double number) {
-        double squared = number * number;
-        return squared * squared;
-    }
-
-    public static String formatResult(double result) {
-        String resultStr = String.format("%.3f", result);
-        return resultStr;
-    }
 }
