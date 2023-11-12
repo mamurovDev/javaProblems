12f60e160985a2fd3877f2f4f4cc2d5e24d07c01
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-12 07:55:35 +0500

    FoP_T10_Practice_11

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,12 @@ public class Main {
 
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int k = scanner.nextInt();
-        boolean[] numbers = new boolean[3];
-        numbers[0] = isMultipleOfK(a, k);
-        numbers[1] = isMultipleOfK(b, k);
-        numbers[2] = isMultipleOfK(c, k);
 
-        for (boolean item : numbers){
-            System.out.print(item + " ");
+        if (a < b){
+            System.out.print(b + " " + a);
         }
-    }
-
-
-    public static boolean isMultipleOfK(int a, int k){
-        return a % k == 0;
+        else {
+            System.out.print(a + " " + b);
+        }
     }
 }
