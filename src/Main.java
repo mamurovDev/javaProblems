b61ba696ef46e9ad1e356d40f2bd1119c44d7d6c
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-12 07:50:57 +0500

    FoP_T10_Practice_11

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double x1 = scanner.nextDouble();
-        double y1 = scanner.nextDouble();
-        double x2 = scanner.nextDouble();
-        double[] numbers = {x1, x2, y1};
-        int countOfNegatives = 0;
-        for (double num : numbers) {
-           if (num < 0){
-               countOfNegatives += 1;
-           }
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int k = scanner.nextInt();
+        boolean[] numbers = new boolean[3];
+        numbers[0] = isMultipleOfK(a, k);
+        numbers[1] = isMultipleOfK(b, k);
+        numbers[2] = isMultipleOfK(c, k);
+
+        for (boolean item : numbers){
+            System.out.print(item + " ");
         }
-        System.out.println(countOfNegatives);
+    }
+
+
+    public static boolean isMultipleOfK(int a, int k){
+        return a % k == 0;
     }
 }
