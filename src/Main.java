6ba57af29bf7a59b47ef29d3d10ffe289216e324
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-15 19:33:02 +0500

    FoP_T10_Practice_15

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double a = scanner.nextDouble();
-        double b = scanner.nextDouble();
-        double c = scanner.nextDouble();
-        double smallest = findSmallest(a, b, c);
-        double largest = findLargest(a, b, c);
-        double sum = (smallest + largest);
-        double roundedValue = roundToThreeDecimals(sum);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
 
-        System.out.printf("%.3f%n", roundedValue);
+        System.out.println(findMax(findMin(a,b), findMin(c, d)));
 
-        scanner.close();
     }
-
-    private static double roundToThreeDecimals(double value) {
-        double scale = 1000.0;
-        return (double) ((int) (value * scale)) / scale;
-    }
-    private static double findSmallest(double a, double b, double c) {
-        return (a <= b && a <= c) ? a : ((b <= a && b <= c) ? b : c);
-    }
-    private static double findLargest(double a, double b, double c) {
-        return (a >= b && a >= c) ? a : ((b >= a && b >= c) ? b : c);
-    }
+    public static int findMax(int a,  int b){
+        return a > b ? a : b;
+    };
+    public static int findMin(int a,  int b){
+        return a < b ? a : b;
+    };
 }
