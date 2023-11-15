517ca62d277737dc033c5720e98a9701d44cd2d4
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-15 19:21:12 +0500

    FoP_T10_Practice_14

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int h = scanner.nextInt();
-        int m = scanner.nextInt();
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double smallest = findSmallest(a, b, c);
+        double largest = findLargest(a, b, c);
+        double sum = (smallest + largest);
+        double roundedValue = roundToThreeDecimals(sum);
 
-        if ( (h >= 0  && h < 24) && (m < 60 && m >= 0)){
-            System.out.println(true);
-        }
-        else {
-            System.out.println(false);
-        }
+        System.out.printf("%.3f%n", roundedValue);
+
+        scanner.close();
+    }
+
+    private static double roundToThreeDecimals(double value) {
+        double scale = 1000.0;
+        return (double) ((int) (value * scale)) / scale;
+    }
+    private static double findSmallest(double a, double b, double c) {
+        return (a <= b && a <= c) ? a : ((b <= a && b <= c) ? b : c);
+    }
+    private static double findLargest(double a, double b, double c) {
+        return (a >= b && a >= c) ? a : ((b >= a && b >= c) ? b : c);
     }
 }
