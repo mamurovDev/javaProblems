54615e37496c35f7dba02e393802d87fb0dda3d3
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-18 10:16:58 +0500

    FoP_T10_2_Practice_15

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;
+
 public class Main {
-    public static void main(String[]  args){
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        long number = scanner.nextLong();
 
-        for (int i = 1; i <= m; i ++){
-            if (m % i == 0){
-                System.out.print(i + " ");
-            }
-        }
+        char mostSignificantDigit = findMostSignificantDigit(number);
+
+        System.out.println(mostSignificantDigit);
+
+        scanner.close();
+    }
+
+    private static char findMostSignificantDigit(long number) {
+        String numberAsString = Long.toString(Math.abs(number));
+
+        char mostSignificantDigit = numberAsString.charAt(0);
+
+        return mostSignificantDigit;
     }
 }
