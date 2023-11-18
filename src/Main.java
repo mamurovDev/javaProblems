49668dd252cdb0705174b0bb5feee0c9babb24d2
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-18 10:06:12 +0500

    FoP_T10_2_Practice_13

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[]  args){
         Scanner sc = new Scanner(System.in);
-        long n = sc.nextInt();
-        long factor = 1;
-       int a = 1;
-        for (long i = n; i  > 0 ; i--){
-            factor = factor * i;
-        }
-
-        System.out.println(factor);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+         for (int i = m; i <= n; i++){
+             char asciiChar = (char) i;
+             System.out.print(asciiChar + " ");
+         }
     }
 }
