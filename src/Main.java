8b3a016892a1c9b8a50ad6579e245ee4d0e6fb24
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-18 09:46:31 +0500

    FoP_T10_2_Practice_12

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,14 @@
 import java.util.Scanner;
-
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        float a = scanner.nextFloat();
-        float b = scanner.nextFloat();
-        float c = scanner.nextFloat();
-        float d = scanner.nextFloat();
-
-
-        double result = roundToThreeDecimals(findMax(d - a, d -b, d - c));
-        System.out.print(String.format("%.3f", result));
-    }
-    private static double roundToThreeDecimals(double value) {
-        double scale = 1000;
-        return (double) ((int) (value * scale)) / scale;
-    }
-    public static double findMax(double a, double b, double c) {
-        if (a >= b && a >= c) {
-            return a;
-        } else if (b >= a && b >= c) {
-            return b;
-        } else {
-            return c;
+    public static void main(String[]  args){
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        long factor = 1;
+       int a = 1;
+        for (long i = n; i  > 0 ; i--){
+            factor = factor * i;
         }
-    };
+
+        System.out.println(factor);
+    }
 }
