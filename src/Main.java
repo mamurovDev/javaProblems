94e16222687dbf514adc977e5e8ed935b56d409a
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-18 10:10:28 +0500

    FoP_T10_2_Practice_14

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
     public static void main(String[]  args){
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
-        int n = sc.nextInt();
-         for (int i = m; i <= n; i++){
-             char asciiChar = (char) i;
-             System.out.print(asciiChar + " ");
-         }
+
+
+        for (int i = 1; i <= m; i ++){
+            if (m % i == 0){
+                System.out.print(i + " ");
+            }
+        }
     }
 }
