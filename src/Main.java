92e464ea84b520ef682133680d02be58597a9840
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-20 08:52:02 +0500

    FoP_T11_Ex04_Arrays

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,21 @@
 public class Main {
 
 
-    public static int getResult(int[] a, int m) {
-        int count = 0;
-        for (int i = 0 ; i < a.length; i++){
-            if (a[i] > m){
-                a[i] = m;
-                count++;
+    public static int[] getResult(int[] a) {
+        int negative = 0;
+        int zero = 0;
+        int positive = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > 0) {
+                positive++;
+            } else if (a[i] < 0) {
+                negative++;
+            } else {
+                zero++;
             }
         }
-        return count;
+
+        return new int[] {negative, zero, positive};
     }
 
 }
