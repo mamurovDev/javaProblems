9fa7f6973761d428f0064c5f0ade1d9feb9f063c
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-20 12:16:06 +0500

    FoP_T11_Ex10_Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,22 @@
+import java.util.Arrays;
+class Main {
 
-public class Main {
+    /**
+     * @param a the given array, containing an even number of elements
+     * @return max(a[0] + a[n - 1], ..., a[n/2 - 1] + a[n/2])
+     */
+    public static int getResult(int[] a) {
+        // Sort the array
+        Arrays.sort(a);
 
-    public static int getResult(int[] a, int c, int d) {
-        int items = 0;
-        for (int num: a){
-            if (c <= num && num <= d){
-                items++;
-            }
+        // Calculate the sum of pairs and find the maximum
+        int maxSum = 0;
+        int n = a.length;
+        for (int i = 0; i < n / 2; i++) {
+            int sum = a[i] + a[n - 1 - i];
+            maxSum = Math.max(maxSum, sum);
         }
-        return items;
+
+        return maxSum;
     }
-
-
-
-
-
 }
