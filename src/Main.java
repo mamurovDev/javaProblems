1c7e8f5745442af5046ff27a714528e000308931
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-20 10:51:03 +0500

    FoP_T11_Ex07_Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
 
 public class Main {
 
+    public static int getResult(int[] a) {
+       int sum = 0;
+       for (int i = 2; i < a.length; i++){
+           if (isPrime(i)){
+               sum += a[i];
+           }
+       }
+       return sum;
+    }
 
-    public static int[] getResult(int[] a) {
-        int[] b = new int[a.length];
-        for (int i = 0; i < a.length; i++){
-            if (a[i] > i){
-                b[i] = 1;
-            } else {
-                b[i] = 0;
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
             }
         }
-        return b;
+        return true;
     }
 
 }
