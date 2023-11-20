0a519e38561ca4afa66da7d5711d0c5e3d4cd58f
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-20 12:10:26 +0500

    FoP_T11_Ex09_Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,18 @@
 
 public class Main {
 
-    public static int getResult(int[] a) {
-       int k1 = findMax(a);
-       int k2 = findMin(a);
-       return  k1 + k2;
-
-    }
-
-    public static int findMax(int[] a){
-        int res = a[0];
-
-        for (int i = 2; i < a.length; i += 2){
-            if (a[i] > res){
-                res = a[i];
+    public static int getResult(int[] a, int c, int d) {
+        int items = 0;
+        for (int num: a){
+            if (c <= num && num <= d){
+                items++;
             }
         }
-
-        return res;
+        return items;
     }
 
-    public static int findMin(int[] a){
-       int initial = a[a.length - 1];
-       for (int i = a.length -1 ; i >= 0; i -= 2){
-           if (a[i] < initial){
-               initial = a[i];
-           }
-       }
-       return initial;
-    }
+
+
+
 
 }
