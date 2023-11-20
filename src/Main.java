310ac5a5677c412b291cff345a7469c3f14bfa67
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-20 08:20:21 +0500

    FoP_T11_Ex02_Arrays

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,13 @@
 public class Main {
 
 
-    public static int[] getResult(int a[], int k) {
-        int[] result = new int[k];
-        int count = 0;
-
-        for (int i = 0; i < a.length && count < k; i++) {
-            if (a[i] > 0) {
-                result[count] = a[i];
-                count++;
+    public static int getResult(int k, int a[]) {
+        int result = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % k == 0) {
+                result = result + a[i];
             }
         }
-
         return result;
     }
 }
