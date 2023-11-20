fc9e406cfd1d39dbc0cb53c87b79265bd418676e
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-20 12:29:15 +0500

    FoP_T11_Ex12_Arrays

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,29 @@ class Main {
      * @param a the given array, containing an even number of elements
      * @return max(a[0] + a[n - 1], ..., a[n/2 - 1] + a[n/2])
      */
-    public static int getResult(int[] a) {
-        if (a.length == 0) {
-            // Handle the case of an empty array
-            return 0;
-        }
+    public static void compress(int[] a) {
+        int n = a.length;
 
-        // Find the minimum element in the array
-        int minElement = a[0];
-        for (int i = 1; i < a.length; i++) {
-            if (a[i] < minElement) {
-                minElement = a[i];
+        // Create a temporary array to store the compressed result
+        int[] result = new int[n];
+
+        // Initialize indices for even and odd positions in the result array
+        int evenIndex = 0;
+        int oddIndex = n / 2;
+
+        // Iterate through the original array and compress elements
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                // Even index, copy the element to the first half
+                result[evenIndex++] = a[i];
+            } else {
+                // Odd index, replace with zero in the second half
+                result[oddIndex++] = 0;
             }
         }
 
-        // Count the number of elements greater than the minimum element
-        int count = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] > minElement) {
-                count++;
-            }
-        }
-
-        return count;
+        // Copy the compressed result back to the original array
+        System.arraycopy(result, 0, a, 0, n);
     }
 
 }
