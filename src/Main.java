9aa2bda7dc4c7ff7db0523ba1f82aed3b3227f70
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2023-11-20 12:26:19 +0500

    FoP_T11_Ex11_Arrays

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,28 @@ class Main {
      * @return max(a[0] + a[n - 1], ..., a[n/2 - 1] + a[n/2])
      */
     public static int getResult(int[] a) {
-        // Sort the array
-        Arrays.sort(a);
-
-        // Calculate the sum of pairs and find the maximum
-        int maxSum = 0;
-        int n = a.length;
-        for (int i = 0; i < n / 2; i++) {
-            int sum = a[i] + a[n - 1 - i];
-            maxSum = Math.max(maxSum, sum);
+        if (a.length == 0) {
+            // Handle the case of an empty array
+            return 0;
         }
 
-        return maxSum;
+        // Find the minimum element in the array
+        int minElement = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < minElement) {
+                minElement = a[i];
+            }
+        }
+
+        // Count the number of elements greater than the minimum element
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > minElement) {
+                count++;
+            }
+        }
+
+        return count;
     }
+
 }
