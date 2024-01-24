4ee6f0b3fd6d9129bb6c161dfe86252857e30860
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2024-01-24 18:36:45 +0500

    FoP_T16_Strings_Task03

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,32 @@
-
-
-
 class Solution {
 
-    public static String lastLongestWord(String text) {
-        String longest = "";
-        //put your code here
-        String[] longest_word = text.split(" ");
-        if (longest_word.length == 1){
-            return longest_word[0];
-        }
+//    /** returns a new one that removes the first and last symbols of words longer than 5 characters.
+//     * @param text given string
+//     * @return the new string
+//     */
 
-        for (int i = 0; i < longest_word.length - 1; i++) {
-            if (longest_word[i].length() > longest_word[i + 1].length()) {
-                longest = longest_word[i];
-            } else {
-                longest = longest_word[i + 1];
-            }
+    public static String remove(String str) {
+        String temp = "";
+        for (int i = 1; i < str.length() - 1; i++) {
+            temp = temp + str.charAt(i);
         }
-        return longest;
+        return temp;
     }
 
+
+
+    static String trimSymbols(String text) {
+        //put your code here
+        String[] arr = text.split(" ");
+        String trimmed = arr[0].length() > 5 ? remove(arr[0]) : arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i].length() > 5) {
+                trimmed += " " + remove(arr[i]);
+            } else {
+                trimmed += " " + arr[i];
+            }
+        }
+
+        return trimmed;
+    }
 }
-
-
-
