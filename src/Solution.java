cf7aaa5a48f6a87ac6d54b8e1e59f25150ba6bc0
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2024-01-25 10:57:24 +0500

    FoP_T16_Strings_Task04

--- src/Solution.java
+++ src/Solution.java
@@ -1,32 +1,30 @@
 class Solution {
 
-//    /** returns a new one that removes the first and last symbols of words longer than 5 characters.
-//     * @param text given string
-//     * @return the new string
+    //    /** returns the number of digits in a string
+//     * @param str the given string
+//     * @return the number of digits in a string
 //     */
 
-    public static String remove(String str) {
-        String temp = "";
-        for (int i = 1; i < str.length() - 1; i++) {
-            temp = temp + str.charAt(i);
-        }
-        return temp;
-    }
 
-
-
-    static String trimSymbols(String text) {
-        //put your code here
-        String[] arr = text.split(" ");
-        String trimmed = arr[0].length() > 5 ? remove(arr[0]) : arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i].length() > 5) {
-                trimmed += " " + remove(arr[i]);
-            } else {
-                trimmed += " " + arr[i];
+    public static boolean ifNumber(String a) {
+        for (int i = 0; i <= 9; i++) {
+            if (a.equals(i + "")) {
+                return true;
             }
         }
 
-        return trimmed;
+        return false;
+    }
+
+    public static int countDigitsInString(String str) {
+        //put your code here
+        int totalNumber = 0;
+        String[] arr = str.split("");
+        for (String s : arr) {
+            if (ifNumber(s)) {
+                totalNumber++;
+            };
+        }
+        return totalNumber;
     }
 }
